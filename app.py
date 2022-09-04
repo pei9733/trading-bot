c6d3e23a380739c5871873ad50eb49eff8f4be16
pei9733 <10040905paron@gmail.com>
2022-09-04 21:17:31 +0800

    beta2.0

--- app.py
+++ app.py
@@ -80,53 +80,53 @@ def welcome():
 # max algo order num
 
 
-@app.route('/test', methods=['POST'])
-def test():
-    # return client.futures_ticker(symbol="BTCUSDT")['lastPrice']  # last price
-    # return client.futures_position_information(symbol="BTCUSDT")  # last price
-    # test_params = {"_side": "SELL", "_quantity": 0.001, "_symbol": "BTCUSDT", "_OrderId": "L_123456789_TEST",
-    #                "_order_type": FUTURE_ORDER_TYPE_MARKET}
-    # order(**test_params)
-    # return json.dumps(order(**test_params))
-    # return json.dumps(client.futures_get_open_orders(symbol="BTCUSDT"))
-    # print(client)
-    # return
-    # start = time.time()
-    # orders = client.futures_get_open_orders(symbol="BTCUSDT")
-    # order_uuids = []
-    # for i in orders:
-    #     i_uuid = i['clientOrderId'][:-2]
-    #     if not (i_uuid in order_uuids):
-    #         order_uuids.append(i_uuid)
-    # # client.futures_stream_close()
-    # end = time.time()
-    # return json.dumps({"time": end - start})
-    # return client.futures_get_open_orders(symbol="BTCUSDT")
-    # return client.futures_get_order(symbol="BTCUSDT", origClientOrderId="justfortest")
-    # return client.futures_exchange_info()     # filter
-    # return client.futures_get_order(
-    #     symbol="BTCUSDT", origClientOrderId="notexist")
-    # return client.futures_cancel_all_open_orders(symbol="BTCUSDT")
-    # return client.futures_account()['assets'][1]['walletBalance']
-    # for i in client.futures_get_all_orders(symbol="BTCUSDT"):
-    #     print(i["clientOrderId"])
-    # return False
-    OrderId = "L"
-    # order_uuid = str(uuid.uuid1().hex)
-    order_uuid = "nevergonna"
-    # order_uuid = "2af4d149291511ed82cf40ec99c99f2c"
-    origClientOrderIdList = json.dumps([OrderId+"_"+order_uuid+"_F", OrderId+"_"+order_uuid +
-                                        "_S", OrderId+"_"+order_uuid+"_O", OrderId+"_"+order_uuid+"_T"]).replace(" ", "")
-    origClientOrderIdList = urllib.parse.quote(origClientOrderIdList)
-    return json.dumps(client.futures_cancel_orders(
-        symbol="BTCUSDT", origClientOrderIdList=origClientOrderIdList))
+# @app.route('/test', methods=['POST'])
+# def test():
+#     # return client.futures_ticker(symbol="BTCUSDT")['lastPrice']  # last price
+#     # return client.futures_position_information(symbol="BTCUSDT")  # last price
+#     # test_params = {"_side": "SELL", "_quantity": 0.001, "_symbol": "BTCUSDT", "_OrderId": "L_123456789_TEST",
+#     #                "_order_type": FUTURE_ORDER_TYPE_MARKET}
+#     # order(**test_params)
+#     # return json.dumps(order(**test_params))
+#     # return json.dumps(client.futures_get_open_orders(symbol="BTCUSDT"))
+#     # print(client)
+#     # return
+#     # start = time.time()
+#     # orders = client.futures_get_open_orders(symbol="BTCUSDT")
+#     # order_uuids = []
+#     # for i in orders:
+#     #     i_uuid = i['clientOrderId'][:-2]
+#     #     if not (i_uuid in order_uuids):
+#     #         order_uuids.append(i_uuid)
+#     # # client.futures_stream_close()
+#     # end = time.time()
+#     # return json.dumps({"time": end - start})
+#     # return client.futures_get_open_orders(symbol="BTCUSDT")
+#     # return client.futures_get_order(symbol="BTCUSDT", origClientOrderId="justfortest")
+#     # return client.futures_exchange_info()     # filter
+#     # return client.futures_get_order(
+#     #     symbol="BTCUSDT", origClientOrderId="notexist")
+#     # return client.futures_cancel_all_open_orders(symbol="BTCUSDT")
+#     # return client.futures_account()['assets'][1]['walletBalance']
+#     # for i in client.futures_get_all_orders(symbol="BTCUSDT"):
+#     #     print(i["clientOrderId"])
+#     # return False
+#     OrderId = "L"
+#     # order_uuid = str(uuid.uuid1().hex)
+#     order_uuid = "nevergonna"
+#     # order_uuid = "2af4d149291511ed82cf40ec99c99f2c"
+#     origClientOrderIdList = json.dumps([OrderId+"_"+order_uuid+"_F", OrderId+"_"+order_uuid +
+#                                         "_S", OrderId+"_"+order_uuid+"_O", OrderId+"_"+order_uuid+"_T"]).replace(" ", "")
+#     origClientOrderIdList = urllib.parse.quote(origClientOrderIdList)
+#     return json.dumps(client.futures_cancel_orders(
+#         symbol="BTCUSDT", origClientOrderIdList=origClientOrderIdList))
 
-    requestedFutures = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'DYDXUSDT']
-    print(
-        {si['symbol']: si['quantityPrecision']
-            for si in info['symbols'] if si['symbol'] in requestedFutures}
-    )
-    return "OK"
+#     requestedFutures = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'DYDXUSDT']
+#     print(
+#         {si['symbol']: si['quantityPrecision']
+#             for si in info['symbols'] if si['symbol'] in requestedFutures}
+#     )
+#     return "OK"
 
 
 @app.route('/test2', methods=['POST'])
@@ -136,11 +136,27 @@ def test2():
     #       origClientOrderId="L_8bf2179126e611edb0c840ec99c99f2c_S")["executedQty"])
     # print(0 == client.futures_get_order(symbol="BTCUSDT",
     #       origClientOrderId="L_8bf2179126e611edb0c840ec99c99f2c_S")["executedQty"])
+    # return json.dumps(cancel_order("BTCUSDT", "L_0b20bc4f29c411eda43b40ec99c99f2c_T"))
     # return json.dumps(client.futures_get_all_orders(symbol="BTCUSDT"))
-    # return client.futures_cancel_all_open_orders(symbol="BTCUSDT")
+    return client.futures_cancel_all_open_orders(symbol="BTCUSDT")
     # return client.futures_get_order(symbol="BTCUSDT", origClientOrderId="web_aByVVd33TXPtH5IbckJW")
     # return client.futures_change_margin_type(symbol="BTCUSDT", marginType="ISOLATED")
-    return client.futures_change_leverage(symbol="BTCUSDT", leverage=10)
+    try:
+        data = json.loads(request.data)
+    except:
+        return json.dumps(["Weird"])
+    try:
+        return data['fdsfadsafdaf']
+    except:
+        return "fuck u"
+
+    # tot = abs(float(client.futures_position_information(
+    #     symbol="BTCUSDT")[0]["positionAmt"]))
+    # if tot == 0:
+    #     print(True)
+    # else:
+    #     print(False)
+    # return client.futures_change_leverage(symbol="BTCUSDT", leverage=10)
     # return client.futures_change_leverage(symbol="ETHUSDT", leverage=10)
     lp = float(client.futures_ticker(symbol="BTCUSDT")['lastPrice'])
     # for i in range(5):
@@ -157,7 +173,19 @@ def test2():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = json.loads(request.data)
+    try:
+        data = json.loads(request.data)
+    except:
+        return json.dumps(["Decline"])
+    try:
+        if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+            print("\n\nPassPhrase Prb\n\n")
+            return {
+                "code": "error",
+                "message": "Invalid passphrase"
+            }
+    except:
+        return json.dumps(["Decline"])
     myOrderType = data['strategy']['alert_message']['orderType']
     symbol = data['ticker'].replace('PERP', '')
     ticksize = 1 if symbol == "BTCUSDT" else 2
@@ -172,13 +200,8 @@ def webhook():
     order_response_TP1 = False
     order_response_TP2 = False
     order_uuid = str(uuid.uuid1().hex)
-    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
-        print("\n\nPassPhrase Prb\n\n")
-        return {
-            "code": "error",
-            "message": "Nice try, invalid passphrase"
-        }
-    elif myOrderType != 'PLACE_ORDER':
+
+    if myOrderType != 'PLACE_ORDER':
         print("\n\n Not PLACE_ORDER \n\n")
         return {
             "code": "error",
@@ -198,7 +221,7 @@ def webhook():
             close_order = order(**order_params_close_all)
         print("\n\nPLACE_ORDER\n\n")
         OrderId = data['strategy']['alert_message']["order_id"].upper()
-        risk = 0.02
+        risk = 0.01
         SL = 0.0
         TP1 = 0.0
         TP2 = 0.0
@@ -236,23 +259,23 @@ def webhook():
             SL_stop_price = round(SL - SL_diff * 0.05, ticksize)
             TP1_stop_price = round(TP1 + SL_diff * 0.05, ticksize)
             TP2_stop_price = round(TP2 + SL_diff * 0.05, ticksize)
-        order_params_PO = {"_side": side, "_quantity": quantity, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid + '_F',
+        order_params_PO = {"_side": side, "_quantity": quantity, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid + '_F',   # First
                            "_price": last_price + price_mod, "_order_type": FUTURE_ORDER_TYPE_LIMIT}
         order_response_PO = order(**order_params_PO)
-        order_params_SL = {"_side": oppsite_side, "_quantity": quantity, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid+'_S',
+        order_params_SL = {"_side": oppsite_side, "_quantity": quantity, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid+'_S',  # Stop Loss
                            "_price": SL, "_stopPrice": SL_stop_price, "_order_type": FUTURE_ORDER_TYPE_STOP}
-        order_params_TP1 = {"_side": oppsite_side, "_quantity": halfQty, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid+'_O',
+        order_params_TP1 = {"_side": oppsite_side, "_quantity": halfQty, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid+'_O',  # One
                             "_price": TP1, "_stopPrice": TP1_stop_price, "_order_type": FUTURE_ORDER_TYPE_TAKE_PROFIT}
-        order_params_TP2 = {"_side": oppsite_side, "_quantity": halfQty, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid+'_T',
+        order_params_TP2 = {"_side": oppsite_side, "_quantity": halfQty, "_symbol": symbol, "_OrderId": OrderId+'_'+order_uuid+'_T',  # Two
                             "_price": TP2, "_stopPrice": TP2_stop_price, "_order_type": FUTURE_ORDER_TYPE_TAKE_PROFIT}
-        for i in range(5):
-            order_response_TP2 = order(**order_params_TP2)
-            if (order_response_TP2[2] and "=-2021" not in str(order_response_TP2[2])) or not order_response_TP2[2]:
-                break
-            time.sleep(1)
         for i in range(5):
             order_response_TP1 = order(**order_params_TP1)
             if (order_response_TP1[2] and "=-2021" not in str(order_response_TP1[2])) or not order_response_TP1[2]:
+                break
+            time.sleep(1)
+        for i in range(5):
+            order_response_TP2 = order(**order_params_TP2)
+            if (order_response_TP2[2] and "=-2021" not in str(order_response_TP2[2])) or not order_response_TP2[2]:
                 break
             time.sleep(1)
         for i in range(5):
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-WEBHOOK_PASSPHRASE = "abcdefgh"
+WEBHOOK_PASSPHRASE = "*NqLUQiLM8Zym3A"
 MONGO_URL = 'mongodb+srv://pei9733:A58013895abc@cluster0.jbqew9b.mongodb.net/?retryWrites=true&w=majority'
 API_KEY = '17f2214edfb0f1e31bd729f7403563ebd487270b46d9bd60071fb5ec9500c02c'
 API_SECRET = '64f110ca4eedb8258967211eec25a9430e23b7e271326c9d2c8f38da46626931'
